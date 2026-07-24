cd759c3e7687c4d14557ac496d0c9f1710445b98
xiaowa112211 <xiaowa632@gmail.com>
2026-07-24 20:19:02 +0200

    Include Firebase Storage in firebase.js
    Added Firebase Storage import and export.

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,20 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";[cite: 3]
+import { getAuth } from "firebase/auth";[cite: 3]
+import { getFirestore } from "firebase/firestore";[cite: 3]
+import { getStorage } from "firebase/storage"; // <-- သစ်ထည့်ပါ
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBsXInos_6ZAbTJRUIsxNKhcW3b6D2VrwI",
-  authDomain: "livechat-df05b.firebaseapp.com",
-  projectId: "livechat-df05b",
-  storageBucket: "livechat-df05b.firebasestorage.app",
-  messagingSenderId: "436491497643",
-  appId: "1:436491497643:web:d7ccae6441ec7b18b287ee",
-  measurementId: "G-T1TV6HW6RG"
+  apiKey: "AIzaSyBsXInos_6ZAbTJRUIsxNKhcW3b6D2VrwI",[cite: 3]
+  authDomain: "livechat-df05b.firebaseapp.com",[cite: 3]
+  projectId: "livechat-df05b",[cite: 3]
+  storageBucket: "livechat-df05b.firebasestorage.app",[cite: 3]
+  messagingSenderId: "436491497643",[cite: 3]
+  appId: "1:436491497643:web:d7ccae6441ec7b18b287ee",[cite: 3]
+  measurementId: "G-T1TV6HW6RG"[cite: 3]
 };
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);[cite: 3]
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const auth = getAuth(app);[cite: 3]
+export const db = getFirestore(app);[cite: 3]
+export const storage = getStorage(app); // <-- Export လုပ်ပါ
